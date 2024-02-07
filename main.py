ee06b5d00a203a03135dcd3f22077a46118c2a68
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2024-02-06 19:18:04 -0600

    .

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def main():
     print("Representación ASCII de la cadena:")
     print(ascii_resultado)
 
-    # Convertir los valores ASCII a su representación binaria manualmente
+    # Convertiendo los valores ASCII a su representación binaria manualmente
     binario_resultado = [ascii_a_binario_manual(valor) for valor in ascii_resultado]
     print("Representación binaria de la cadena:")
     print(binario_resultado)
 
-    # Convertir representación binaria a la cadena original
+    # Convertiendo la representación binaria a la cadena original
     cadena_original = binario_a_cadena(''.join(binario_resultado))
     print("Cadena original obtenida de la representación binaria:")
     print(cadena_original)
